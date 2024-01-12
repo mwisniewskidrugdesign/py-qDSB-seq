1b55af4f0eacf66074f8409f666bb485ecf27cfe
maciejwisniewski <m.wisniewski@datascience.edu.pl>
2024-01-12 12:33:00 +0100

    genome_sequence_length

--- qDSB-seq.sh
+++ qDSB-seq.sh
@@ -12,15 +12,16 @@ if [ "$#" -lt 2 ]; then
 	Usage: bash $0 <DSB reads> <gDNA reads R1> <gDNA reads R2> [options] 
 
 	    -s CHARACTER	sample name
-	    -r CHARACTER        group of the sample, for example, G1 or S
+	    -r CHARACTER  group of the sample, for example, G1 or S
 	    -g CHARACTER	genome name
-	    -f CHARACTER        genome sequence in fasta format
+	    -f CHARACTER  genome sequence in fasta format
 	    -i CHARACTER	genome bowtie index
 	    -e CHARACTER	enzyme name
-	    -t CHARACTER        enzyme type
-	    -c CHARACTER        enzyme cutting sites in BED format
-	    -b CHARACTER        backgrond coordinates on genome to remove background noise from cutting efficiency calculation
-	    -p CHARACTER        output prefix
+	    -t CHARACTER  enzyme type
+	    -c CHARACTER  enzyme cutting sites in BED format
+	    -b CHARACTER  backgrond coordinates on genome to remove background noise from cutting efficiency calculation
+	    -p CHARACTER  output prefix
+	    -o CHARACTER  output direcctory
 
 	Please contact Dr. Yingjie Zhu for any questions on the code yizhu\@utmb.edu
 
@@ -28,7 +29,7 @@ if [ "$#" -lt 2 ]; then
     exit 1
 fi
 
-USAGE="$0 $1 $2 $3 [-s <s> -r <r> -g <g> -f <f> -i <i> -e <e> -t <t> -c <c> -b <b> -p <p>]"
+USAGE="$0 $1 $2 $3 [-s <s> -r <r> -g <g> -f <f> -i <i> -e <e> -t <t> -c <c> -b <b> -p <p> -o <o>]"
 
 DSB_reads=$1
 gDNA_reads_1=$2
@@ -45,9 +46,10 @@ enzyme_type="3"
 enzyme_cutting_sites=""
 background=""
 prefix="output"
+output_dir=''
 
 
-while getopts 's:r:g:f:i:e:t:c:b:p:' opt ; do
+while getopts 's:r:g:f:i:e:t:c:b:p:o:' opt ; do
 	case $opt in
 		s) sample_name="$OPTARG";;
 		r) group_sample="$OPTARG" ;;
@@ -59,6 +61,7 @@ while getopts 's:r:g:f:i:e:t:c:b:p:' opt ; do
 		c) enzyme_cutting_sites="$OPTARG" ;;
 		b) background="$OPTARG" ;;
 		p) prefix="$OPTARG" ;;
+    o) output_dir="$OPTARG" ;;
 		\?) echo "Invalid option: -$OPTARG"; exit 1 ;;
 		:) echo "Option -$OPTARG requires an argument."; exit 1 ;;
 	esac
@@ -78,12 +81,21 @@ echo "enzyme_type: $enzyme_type"
 echo "enzyme_cutting_sites: $enzyme_cutting_sites"
 echo "background: $background"
 echo "prefix: $prefix"
+echo "output_directory: $output_dir"
+
+results_output_dir=$output_dir'/results'
+mkdir $results_output_dir
 
 chr_length=${#genome_sequence}
 
 if [ -n "$genome_sequence" ]; then
-	python3 scripts/fasta_length.py "$genome_sequence" #>"genome_sequence.length" 2>"genome_sequence.length.bed"
-	#$Bin/PERL/fastaLength.pl "$genome_sequence" > "$genome_sequence.length" 2>"$genome_sequence.length.bed"
+  genome_sequence_output_dir=$output_dir'/results/genome_sequence'
+  mkdir $genome_sequence_output_dir
+
+  genome_sequence_length_file=$genome_sequence_output_dir'/genome_sequence.length'
+  genome_sequence_length_bed_file=$genome_sequence_output_dir'/genome_sequence.length.bed'
+
+	python3 scripts/fasta_length.py "$genome_sequence" >$genome_sequence_length_file 2>$genome_sequence_length_bed_file
 fi
 
 
